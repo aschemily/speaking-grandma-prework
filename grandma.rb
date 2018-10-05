0e7908124d850fa69551ff3b25f3d3ec82b261e8
Emily Asch <e.asch@me.com>
2018-10-05 20:55:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,10 @@ def speak_to_grandma(speak)
   if speak != speak.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif speak == speak.upcase
-    return "NO, NOT SINCE 1938!"
-  else
+    
     return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
     
   end
 end
